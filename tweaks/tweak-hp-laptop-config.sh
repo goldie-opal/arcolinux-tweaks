9a4fba5265f165250c8cef38888d45ca36e5e519
Goldie Opal <34083352+goldie-opal@users.noreply.github.com>
2019-02-27 22:26:39 +1000

    Updated packages to install

--- tweaks/tweak-hp-laptop-config.sh
+++ tweaks/tweak-hp-laptop-config.sh
@@ -6,13 +6,6 @@ set -e
 #
 ##################################################################################################################
 
-# Add my repo
-echo '
-[arcolinux_repo_go]
-SigLevel = PackageOptional DatabaseOptional
-Server = https://goldie-opal.github.io/$repo/$arch
-
-' | sudo tee --append /etc/pacman.conf
 
 # Set pacman mirrors
 sudo cp config/mirrorlist /etc/pacman.d/mirrorlist
@@ -46,6 +39,6 @@ sudo systemctl stop reflector.timer
 sudo systemctl disable reflector.timer
 
 # Install my packages
-sudo pacman -S --needed --noconfirm xappearance-gtk3 archway-theme-git sardi-ghost-flexible-archway-git arcolinux-wallpapers-go-git
-yay -S ttf-pizzadude-bullets youtube-dl-gui-git ttf-mac-fonts tamzen-font-git github-desktop-bin
+sudo pacman -S --needed --noconfirm xappearance-gtk3 
+yay -S youtube-dl-gui-git ttf-mac-fonts tamzen-font-git github-desktop-bin mint-themes mint-y-icons mint-x-icons
 
