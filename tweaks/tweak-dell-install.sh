12c077b8f9b0cdb96d7687eddd34da2f4c8a5c3e
Goldie Opal <34083352+goldie-opal@users.noreply.github.com>
2019-02-28 22:24:10 +1000

    Added script to install extra packages

--- tweaks/tweak-dell-install.sh
+++ tweaks/tweak-dell-install.sh
@@ -5,13 +5,6 @@ set -e
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
-# Add my repo
-echo '
-[arcolinux_repo_go]
-SigLevel = PackageOptional DatabaseOptional
-Server = https://goldie-opal.github.io/$repo/$arch
-' | sudo tee --append /etc/pacman.conf
-
 
 # Set pacman mirrors
 sudo cp config/mirrorlist /etc/pacman.d/mirrorlist
@@ -32,7 +25,3 @@ yay conky-lua-archers
 # Disable reflector service when using local cache
 sudo systemctl stop reflector.timer
 sudo systemctl disable reflector.timer
-
-# Install my packages
-sudo pacman -S --needed --noconfirm xappearance-gtk3 archway-theme-git sardi-ghost-flexible-archway-git arcolinux-wallpapers-go-git
-yay -S youtube-dl-gui-git ttf-mac-fonts tamzen-font-git mint-themes mint-y-icons mint-x-icons
--- tweaks/tweak-hp-laptop-config.sh
+++ tweaks/tweak-hp-laptop-config.sh
@@ -5,12 +5,6 @@ set -e
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
-# Add my repo
-echo '
-[arcolinux_repo_go]
-SigLevel = PackageOptional DatabaseOptional
-Server = https://goldie-opal.github.io/$repo/$arch
-' | sudo tee --append /etc/pacman.conf
 
 # Set pacman mirrors
 sudo cp config/mirrorlist /etc/pacman.d/mirrorlist
@@ -42,8 +36,3 @@ sudo cp config/rtbth.conf /etc/modules-load.d/
 # Disable reflector service 
 sudo systemctl stop reflector.timer
 sudo systemctl disable reflector.timer
-
-# Install my packages
-sudo pacman -S --needed --noconfirm xappearance-gtk3 archway-theme-git sardi-ghost-flexible-archway-git arcolinux-wallpapers-go-git
-yay -S youtube-dl-gui-git ttf-mac-fonts tamzen-font-git github-desktop-bin mint-themes mint-y-icons mint-x-icons
-
