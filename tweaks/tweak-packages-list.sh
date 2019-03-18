e9e6321dc7429760526648d1d8e39668bf6f0160
Goldie Opal <34083352+goldie-opal@users.noreply.github.com>
2019-03-18 18:44:48 +1000

    Added packages list tweak

---
+++ tweaks/tweak-packages-list.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+###################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+# get arco-xfce from git
+git clone https://github.com/arcolinuxb/arco-xfce
+
+# comment out unwanted packages in arco-xfce/archiso/packages.x86_64
+sed -i '/^chromium/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^variety/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^arco-variety/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^geany/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^arco-geany/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^arcolinux-arc-themes-git/s/^/#/' arco-xfce/archiso/packages.x86_64
+
+echo lxappearance-gtk3 >> arco-xfce/archiso/packages.x86_64
+echo arcolinux-arc-themes-nico-git  >> arco-xfce/archiso/packages.x86_64
