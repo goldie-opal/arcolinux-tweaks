873785c2a175c7b3100465e6927d3fee6080729d
Goldie Opal <34083352+goldie-opal@users.noreply.github.com>
2019-03-21 22:40:02 +1000

    Update arcob-xfce package list

--- arcob-xfce-tweaks/tweak-packages-list-xfce.sh
+++ arcob-xfce-tweaks/tweak-packages-list-xfce.sh
@@ -22,5 +22,11 @@ sed -i '/^vim/s/^/#/' arco-xfce/archiso/packages.x86_64
 sed -i '/^atom/s/^/#/' arco-xfce/archiso/packages.x86_64
 sed -i '/^apm/s/^/#/' arco-xfce/archiso/packages.x86_64
 sed -i '/^electron/s/^/#/' arco-xfce/archiso/packages.x86_64
-#echo lxappearance-gtk >> arco-xfce/archiso/packages.x86_64
+sed -i '/^nss-mdns/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^xterm/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^simple-scan/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^ristretto/s/^/#/' arco-xfce/archiso/packages.x86_64
+
+# add extra packages
+echo lxappearance-gtk3 >> arco-xfce/archiso/packages.x86_64
 echo arcolinux-arc-themes-nico-git  >> arco-xfce/archiso/packages.x86_64
