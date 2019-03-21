e7751c500cdf618d206254f1946287d71d0ac672
Goldie Opal <34083352+goldie-opal@users.noreply.github.com>
2019-03-21 20:47:44 +1000

    Updated arcob-xfce packages

--- arcob-xfce-tweaks/tweak-packages-list-xfce.sh
+++ arcob-xfce-tweaks/tweak-packages-list-xfce.sh
@@ -12,12 +12,15 @@ git clone https://github.com/arcolinuxb/arco-xfce
 # comment out unwanted packages in arco-xfce/archiso/packages.x86_64
 sed -i '/^chromium/s/^/#/' arco-xfce/archiso/packages.x86_64
 sed -i '/^variety/s/^/#/' arco-xfce/archiso/packages.x86_64
-sed -i '/^arco-variety/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^arcolinux-variety/s/^/#/' arco-xfce/archiso/packages.x86_64
 sed -i '/^geany/s/^/#/' arco-xfce/archiso/packages.x86_64
-sed -i '/^arco-geany/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^arcolinux-geany/s/^/#/' arco-xfce/archiso/packages.x86_64
 sed -i '/^arcolinux-arc-themes-git/s/^/#/' arco-xfce/archiso/packages.x86_64
 sed -i '/^vivaldi/s/^/#/' arco-xfce/archiso/packages.x86_64
 sed -i '/^vivaldi-codecs-ffmpeg-extra-bin/s/^/#/' arco-xfce/archiso/packages.x86_64
-
-echo lxappearance-gtk3 >> arco-xfce/archiso/packages.x86_64
+sed -i '/^vim/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^atom/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^apm/s/^/#/' arco-xfce/archiso/packages.x86_64
+sed -i '/^electron/s/^/#/' arco-xfce/archiso/packages.x86_64
+#echo lxappearance-gtk >> arco-xfce/archiso/packages.x86_64
 echo arcolinux-arc-themes-nico-git  >> arco-xfce/archiso/packages.x86_64
