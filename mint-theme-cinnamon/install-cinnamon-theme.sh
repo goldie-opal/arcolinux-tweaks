3f8a299bd574c50cdecb588fbdba50a840241da2
Goldie Opal <34083352+goldie-opal@users.noreply.github.com>
2019-05-02 22:53:41 +1000

    Update mint theme scripts

--- mint-theme-cinnamon/install-cinnamon-theme.sh
+++ mint-theme-cinnamon/install-cinnamon-theme.sh
@@ -7,6 +7,6 @@ set -e
 ##################################################################################################################
 
 #yay mint-themes
-sudo pacman -S mint-x-icons-1.5.1-1-any.pkg.tar.xz mint-y-icons-1.3.3-1-any.pkg.tar.xz
-sudo pacman -S mint-themes-1.7.8-1-any.pkg.tar.xz
+sudo pacman -U mint-x-icons-1.5.1-1-any.pkg.tar.xz mint-y-icons-1.3.3-1-any.pkg.tar.xz
+sudo pacman -U mint-themes-1.7.8-1-any.pkg.tar.xz
 dconf load /org/cinnamon/ < cinnamon_desktop_backup
