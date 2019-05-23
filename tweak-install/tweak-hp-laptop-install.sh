16b30eb919ee4525731e4f21330dca91f8596b81
Goldie Opal <34083352+goldie-opal@users.noreply.github.com>
2019-05-23 18:06:04 +1000

    Updated laptop and pc tweaks

--- tweak-install/tweak-hp-laptop-install.sh
+++ tweak-install/tweak-hp-laptop-install.sh
@@ -6,57 +6,83 @@ set -e
 #
 ##################################################################################################################
 
-# Install intel microcode
-sudo pacman -S --noconfirm intel-ucode
-sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-# Install nVidia drivers and Bumblebee
-sudo pacman -S --noconfirm --needed nvidia bumblebee nvidia-utils bbswitch primus lib32-virtualgl lib32-nvidia-utils lib32-primus nvidia-settings
-sudo gpasswd -a $USER bumblebee
-sudo systemctl enable bumblebeed.service
+function installIntelUcode() {
+	# Install intel microcode
+	sudo pacman -S --noconfirm intel-ucode
+	sudo grub-mkconfig -o /boot/grub/grub.cfg
+}
 
-# Configure hw clock to use localtime for dual booting
-sudo timedatectl set-local-rtc 1 
-sudo hwclock --systohc --local
 
-# Copy script to reset lan after waking
-sudo touch /usr/lib/systemd/system-sleep/reset_lan_after_sleep.sh 
-cat  <<'EOT' | sudo tee /usr/lib/systemd/system-sleep/reset_lan_after_sleep.sh 
-#!/bin/bash
-case $1/$2 in
-	pre/*)
-	;;
-	post/*)
-	 modprobe -r r8169
-         modprobe r8169
-	;;
-esac
+function installNvidiaDrivers() {
+	# Install nVidia drivers and Bumblebee
+	sudo pacman -S --noconfirm --needed nvidia bumblebee nvidia-utils bbswitch primus lib32-virtualgl lib32-nvidia-utils lib32-primus nvidia-settings
+	sudo gpasswd -a $USER bumblebee
+	sudo systemctl enable bumblebeed.service
+}
+
+
+function setHardwareClock() {
+	# Configure hw clock to use localtime for dual booting
+	sudo timedatectl set-local-rtc 1 
+	sudo hwclock --systohc --local
+}
+
+
+function resetLAN() {
+	# Copy script to reset lan after waking
+	sudo touch /usr/lib/systemd/system-sleep/reset_lan_after_sleep.sh 
+	cat  <<'EOT' | sudo tee /usr/lib/systemd/system-sleep/reset_lan_after_sleep.sh 
+	#!/bin/bash
+	case $1/$2 in
+		pre/*)
+		;;
+		post/*)
+		 modprobe -r r8169
+	         modprobe r8169
+		;;
+	esac
 EOT
-sudo chmod +x /usr/lib/systemd/system-sleep/reset_lan_after_sleep.sh 
+	sudo chmod +x /usr/lib/systemd/system-sleep/reset_lan_after_sleep.sh 
+}
 
-# Install Bluetooth Driver -  may be causing issues with latest kernels
-#sudo pacman --needed --noconfirm -S linux-headers dkms
-#yay -S rtbth-dkms-git
-#sudo touch /etc/modules-load.d/rtbth.conf
-#sudo su -c 'echo -e "rtbth" > /etc/modules-load.d/rtbth.conf'
 
-# Fix dns
-sudo pacman -S systemd-resolvconf
+function installBlueToothDriver() {
+	# Install Bluetooth Driver -  may be causing issues with latest kernels
+	sudo pacman --needed --noconfirm -S linux-headers dkms
+	yay -S rtbth-dkms-git
+	sudo touch /etc/modules-load.d/rtbth.conf
+	sudo su -c 'echo -e "rtbth" > /etc/modules-load.d/rtbth.conf'
+}
 
-# System tweaks
-# 8 Cores
-sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j9"/g' /etc/makepkg.conf
-sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 8 -z -)/g' /etc/makepkg.conf
 
-# Faster shutdown
-FIND="use_lvmetad = 1"
-REPLACE="use_lvmetad = 0"
-find /etc/lvm/lvm.conf -type f -exec sudo sed -i "s/$FIND/$REPLACE/g" {} \;
-sudo systemctl stop lvm2-lvmetad.socket lvm2-lvmetad.service
-sudo systemctl disable lvm2-lvmetad.socket lvm2-lvmetad.service
-sudo systemctl mask lvm2-monitor
+function applyTweaks() {
+	# Fix dns
+	sudo pacman -S systemd-resolvconf
 
-#Enable periodic trim
-sudo sed -i 's/,discard / /g' /etc/fstab
-sudo systemctl enable fstrim.timer
-sudo systemctl start fstrim.timer
+	# System tweaks
+	# 8 Cores
+	sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j9"/g' /etc/makepkg.conf
+	sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T 8 -z -)/g' /etc/makepkg.conf
+
+	# Faster shutdown
+	FIND="use_lvmetad = 1"
+	REPLACE="use_lvmetad = 0"
+	find /etc/lvm/lvm.conf -type f -exec sudo sed -i "s/$FIND/$REPLACE/g" {} \;
+	sudo systemctl stop lvm2-lvmetad.socket lvm2-lvmetad.service
+	sudo systemctl disable lvm2-lvmetad.socket lvm2-lvmetad.service
+	sudo systemctl mask lvm2-monitor
+
+	#Enable periodic trim
+	sudo sed -i 's/,discard / /g' /etc/fstab
+	sudo systemctl enable fstrim.timer
+	sudo systemctl start fstrim.timer
+
+}
+
+installIntelUcode
+installNvidiaDrivers
+setHardwareClock
+#resetLAN
+#installBlueToothDriver
+applyTweaks
--- tweak-install/tweak-install-dell.sh
+++ tweak-install/tweak-install-dell.sh
@@ -6,29 +6,41 @@ set -e
 #
 ##################################################################################################################
 
-# Install intel microcode
-sudo pacman -S --noconfirm intel-ucode
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+function installIntelUcode() {
+	# Install intel microcode
+	sudo pacman -S --noconfirm intel-ucode
+	sudo grub-mkconfig -o /boot/grub/grub.cfg
+}
 
-# Configure hw clock to use localtime for dual booting
-sudo timedatectl set-local-rtc 1 
-sudo hwclock --systohc --local
+function setHardwareClock() {
+	# Configure hw clock to use localtime for dual booting
+	sudo timedatectl set-local-rtc 1 
+	sudo hwclock --systohc --local
+}
 
-# Install nVidia drivers 
-if pacman -Qi "conky-lua-archers" &> /dev/null; then
-	sudo pacman -R --noconfirm conky-lua-archers libxnvctrl
-fi
-sudo pacman -S --noconfirm --needed nvidia-340xx nvidia-340xx-utils lib32-virtualgl lib32-nvidia-340xx-utils libxnvctrl-340xx
+function installNvidiaDrivers() {
+	# Install nVidia drivers 
+	if pacman -Qi "conky-lua-archers" &> /dev/null; then
+		sudo pacman -R --noconfirm conky-lua-archers libxnvctrl
+	fi
+	sudo pacman -S --noconfirm --needed nvidia-340xx nvidia-340xx-utils lib32-virtualgl lib32-nvidia-340xx-utils libxnvctrl-340xx
+}
 
 
-# Faster shutdown
-FIND="use_lvmetad = 1"
-REPLACE="use_lvmetad = 0"
-find /etc/lvm/lvm.conf -type f -exec sudo sed -i "s/$FIND/$REPLACE/g" {} \;
-sudo systemctl stop lvm2-lvmetad.socket lvm2-lvmetad.service
-sudo systemctl disable lvm2-lvmetad.socket lvm2-lvmetad.service
-sudo systemctl mask lvm2-monitor
+function applyTweaks() {
+	# Fix dns
+	sudo pacman -S systemd-resolvconf
 
-# Fix dns
-sudo pacman -S systemd-resolvconf
+	# Faster shutdown
+	FIND="use_lvmetad = 1"
+	REPLACE="use_lvmetad = 0"
+	find /etc/lvm/lvm.conf -type f -exec sudo sed -i "s/$FIND/$REPLACE/g" {} \;
+	sudo systemctl stop lvm2-lvmetad.socket lvm2-lvmetad.service
+	sudo systemctl disable lvm2-lvmetad.socket lvm2-lvmetad.service
+	sudo systemctl mask lvm2-monitor
+}
 
+installIntelUcode
+installNvidiaDrivers
+setHardwareClock
+applyTweaks
