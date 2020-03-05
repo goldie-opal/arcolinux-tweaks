dd037821b0132edad313eb4e320a1d3e52b86052
Goldie Opal <34083352+goldie-opal@users.noreply.github.com>
2020-03-05 20:18:25 +1000

    Added Manjaro theme

---
+++ themes/manjaro-theme/install-manjaro-theme.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+###################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+if pacman -Qi "mint-x-icons" &> /dev/null; then
+	dconf load /org/cinnamon/ < mint-y-aqua
+else
+	yay -S --noconfirm mint-x-icons
+	yay -S --noconfirm mint-y-icons
+	yay -S --noconfirm mint-themes
+	yay -S --noconfirm papirus-maia-icon-theme-git
+	dconf load /org/cinnamon/ < mint-y-dark-teal
+fi
