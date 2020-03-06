e8bf568046c7b72a31a0dca5fea0c0c0bf4bffff
Goldie Opal <34083352+goldie-opal@users.noreply.github.com>
2020-03-06 21:25:54 +1000

    Added gaming

--- tweak-install/tweak-hp-laptop-install.sh
+++ tweak-install/tweak-hp-laptop-install.sh
@@ -72,7 +72,7 @@ function applyTweaks() {
 	~/.bin/main/000-use-all-cores-makepkg-conf-v4.sh
 	sudo sed -i 's/loglevel=/udev.log_priority=/g' /etc/defaults/grub
 	sudo grub-mkconfig -o /boot/grub/grub.cfg
-	sudo sed -i 's/ fsck / /g' /etc/mkinitcpio.conf
+	sudo sed -i 's/ fsck/ /g' /etc/mkinitcpio.conf
 	sudo mkinitcpio -p linux
 }
 
@@ -107,8 +107,14 @@ function installMintTheme() {
 	fi
 }
 
+function Gaming() {
+	sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader wine-gecko wine-mono
+	sudo pacman -S arcolinux-meta-steam lutris
+}
+
+
 updateMirrors
-#installIntelUcode
+installIntelUcode
 #installNvidiaDrivers
 installNvidiaDriversOptimusManager
 setHardwareClock
@@ -118,3 +124,4 @@ installBlueToothDriver
 setPeriodicTrim
 applyTweaks
 installMintTheme
+Gaming
